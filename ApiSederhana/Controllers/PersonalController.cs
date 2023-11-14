c6483e71d35f4c1e8049e0e6d39953a00b6065c3
Sawaluddin <sawaluddinsrg472@gmail.com>
2023-11-14 17:11:16 +0700

    Add project files.

---
+++ ApiSederhana/Controllers/PersonalController.cs
@@ -0,0 +1,58 @@
+﻿using ApiSederhana.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSederhana.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonalController : ControllerBase
+    {
+        private static List<Personal> personal = new List<Personal>()
+        {
+            new Personal()
+            {
+                Umur = 28,
+                Name = "Yanti PWK",
+                //Alamat = "Ciamis",
+                Address = new Alamat{Kecamatan = "Kawali",Kota = "Ciamis",Provinsi = "Jawa Barat"},
+                Skill = new List<string>{ "JavaScript","Kalkulus"}
+            },
+            new Personal()
+            {
+                Umur = 52,
+                Name = "Pak Iwan",
+                //Alamat = "Bandung",
+                Address = new Alamat{Kecamatan = "Cicendo",Kota = "Bandung",Provinsi = "Jawa Barat"},
+                Skill = new List<string>{ "Marah-marah","Photographer"}
+            },
+            new Personal()
+            {
+                Umur = 30,
+                Name = "Pak Nyoman",
+                //Alamat = "Pasteur",
+                Address = new Alamat{Kecamatan = "Pasteur",Kota = "Bandung",Provinsi = "Jawa Barat"},
+                Skill = new List<string>{ "Kemanan","Bulu Tangkis"}
+            },
+            new Personal()
+            {
+                Umur = 45,
+                Name = "Pak Dany",
+                //Alamat = "Cimindi",
+                Address = new Alamat{Kecamatan = "Cimindi",Kota = "Bandung",Provinsi = "Jawa Barat"},
+                Skill = new List<string>{ "Mudah ramah","Mudah marah"}
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Personal> GET()
+        {
+            return personal;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Personal personal1)
+        {
+            personal.Add(personal1);
+        }
+    }
+}
---
+++ ApiSederhana/Models/Alamat.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiSederhana.Models
+{
+    public class Alamat
+    {
+        public string Kecamatan { get; set; }
+
+        public string Kota { get; set; }
+
+        public string Provinsi { get; set; }
+    }
+}
